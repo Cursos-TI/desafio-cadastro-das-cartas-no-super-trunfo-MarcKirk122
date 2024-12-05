bf96730977fb4119c76ccafa4c4836e24b842738
Marcos Silva <marcos.augsilva@gmail.com>
2024-12-05 00:23:29 +0000

    avancado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,12 +2,15 @@
 
 //Criação de struct que representa as Cartas
 typedef struct CartaCidade {
+    char estado[20];
     int codigo;
     char nome[30];
     int populacao;
-    float area;
+    double area;
     double pib;
     int numeroPontosTuristicos;
+    double densidadePopulacional;
+    double pibPerCapita;
 } CartaCidade;
 
 int main() {
@@ -16,6 +19,8 @@ int main() {
 
     //Leitura dos dados
     printf("Bem vindo ao Super Trunfo - Países!\n\n");
+    printf("Digite o estado: ");
+    scanf("%d", cartaCidade.estado);
     printf("Digite o código da cidade: ");
     scanf("%d", &cartaCidade.codigo);
     printf("Digite o nome da cidade: ");
@@ -23,18 +28,24 @@ int main() {
     printf("Digite a população: ");
     scanf("%d", &cartaCidade.populacao);
     printf("Digite a área: ");
-    scanf("%f", &cartaCidade.area);
+    scanf("%lf", &cartaCidade.area);
     printf("Digite o PIB: ");
     scanf("%lf", &cartaCidade.pib);
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &cartaCidade.numeroPontosTuristicos);
 
+    cartaCidade.densidadePopulacional = cartaCidade.populacao/cartaCidade.area;
+    cartaCidade.pibPerCapita = cartaCidade.pib/cartaCidade.populacao;
+    
     //Impressão dos dados
+    printf("\nEstado: %d\n", cartaCidade.estado);
     printf("\nCódigo da cidade: %d\n", cartaCidade.codigo);
     printf("Nome da cidade: %s\n", cartaCidade.nome);
     printf("População da cidade: %d\n", cartaCidade.populacao);
-    printf("Área da cidade: %.2f\n", cartaCidade.area);
-    printf("PIB da cidade: %.2lf\n", cartaCidade.pib);
+    printf("Área da cidade: %.2lf\n km²", cartaCidade.area);
+    printf("Densidade Populacional: %.2lf\n pessoas/km²", cartaCidade.densidadePopulacional);
+    printf("PIB da cidade: %.2lf\n bilhões de reais", cartaCidade.pib);
+    printf("PIB per Capita: %.2lf\n reais", cartaCidade.pibPerCapita);
     printf("Número de pontos turísticos: %d\n", cartaCidade.numeroPontosTuristicos);
 
     return 0;
