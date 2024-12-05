8625957a42743cc1ae158350f458cebb1146d3d4
Marcos Silva <marcos.augsilva@gmail.com>
2024-12-05 00:30:47 +0000

    aa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,10 +2,10 @@
 
 //Criação de struct que representa as Cartas
 typedef struct CartaCidade {
-    char estado[20];
-    int codigo;
+    char estado;
+    char codigo[5];
     char nome[30];
-    int populacao;
+    long int populacao;
     double area;
     double pib;
     int numeroPontosTuristicos;
@@ -20,13 +20,13 @@ int main() {
     //Leitura dos dados
     printf("Bem vindo ao Super Trunfo - Países!\n\n");
     printf("Digite o estado: ");
-    scanf("%d", cartaCidade.estado);
+    scanf("%c", &cartaCidade.estado);
     printf("Digite o código da cidade: ");
-    scanf("%d", &cartaCidade.codigo);
+    scanf("%s", cartaCidade.codigo);
     printf("Digite o nome da cidade: ");
     scanf("%s", cartaCidade.nome);
     printf("Digite a população: ");
-    scanf("%d", &cartaCidade.populacao);
+    scanf("%li", &cartaCidade.populacao);
     printf("Digite a área: ");
     scanf("%lf", &cartaCidade.area);
     printf("Digite o PIB: ");
@@ -38,14 +38,14 @@ int main() {
     cartaCidade.pibPerCapita = cartaCidade.pib/cartaCidade.populacao;
     
     //Impressão dos dados
-    printf("\nEstado: %d\n", cartaCidade.estado);
-    printf("\nCódigo da cidade: %d\n", cartaCidade.codigo);
+    printf("\nEstado: %c\n", cartaCidade.estado);
+    printf("\nCódigo da cidade: %s\n", cartaCidade.codigo);
     printf("Nome da cidade: %s\n", cartaCidade.nome);
-    printf("População da cidade: %d\n", cartaCidade.populacao);
-    printf("Área da cidade: %.2lf\n km²", cartaCidade.area);
-    printf("Densidade Populacional: %.2lf\n pessoas/km²", cartaCidade.densidadePopulacional);
-    printf("PIB da cidade: %.2lf\n bilhões de reais", cartaCidade.pib);
-    printf("PIB per Capita: %.2lf\n reais", cartaCidade.pibPerCapita);
+    printf("População da cidade: %li\n", cartaCidade.populacao);
+    printf("Área da cidade: %.2lf km²\n", cartaCidade.area);
+    printf("Densidade Populacional: %.2lf pessoas/km²\n", cartaCidade.densidadePopulacional);
+    printf("PIB da cidade: %.2lf bilhões de reais\n", cartaCidade.pib);
+    printf("PIB per Capita: %.2lf reais\n", cartaCidade.pibPerCapita);
     printf("Número de pontos turísticos: %d\n", cartaCidade.numeroPontosTuristicos);
 
     return 0;
